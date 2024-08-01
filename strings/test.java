7bd720551565f9fdb44a3d2253b8f7d714f157fe
gitshubhsss <shubhamranjane16@gmail.com>
2024-08-01 22:34:53 +0530

    .\test.java

---
+++ strings/test.java
@@ -0,0 +1,17 @@
+
+
+public class test {
+    
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            
+            for(int j=1;j<=10;j++){
+                if(j==2){
+                    System.out.println(j);
+                    break;
+                }
+            }
+           
+        }
+    }
+}
