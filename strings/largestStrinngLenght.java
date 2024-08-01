9de9521f3c95652d77c72ffe9061086594c08820
gitshubhsss <shubhamranjane16@gmail.com>
2024-08-01 22:33:43 +0530

    .\largestStrinngLenght.java

---
+++ strings/largestStrinngLenght.java
@@ -0,0 +1,18 @@
+
+
+public class largestStrinngLenght {
+    public static int lengthOfLongestSubstring(String s) {
+
+        int maxLenth=Integer.MIN_VALUE;
+        int length=0;
+        int n=s.length();
+        for(int i=0;i<n-1;i++){
+            
+        }
+        return maxLenth;
+    }
+    public static void main(String[] args) {
+        String s="pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));;
+    }
+}
